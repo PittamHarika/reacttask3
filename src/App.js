c58ab923730bb77c1e527ab32d22a2e04547766f
PittamHarika <pittamharika04@gmail.com>
2023-10-15 15:59:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {palette} from './components/palette';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <palette />
+    </div>
+  );
+}
+
+export default App;
